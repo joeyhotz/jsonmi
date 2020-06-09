e7bce595962d760cb2806edc4217da97f8bafcb0
Joey Hotz <joeyhotz1@gmail.com>
2020-06-09 20:08:55 +1000

    adds tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import JsonBox from "./Components/JsonBox/JsonBox";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { IoIosHelpCircle } from "react-icons/io";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function App() {
   return (
@@ -23,6 +25,12 @@ function App() {
           paddingBottom: "20px",
         }}
       >
+        <OverlayTrigger placement="left" delay={{ show: 100, hide: 100 }} overlay={renderTooltip}>
+          <div style={{ position: "absolute", top: 30, right: 30 }}>
+            <IoIosHelpCircle></IoIosHelpCircle>
+          </div>
+        </OverlayTrigger>
+
         <h1 style={{ fontWeight: 600 }}>jsonmi</h1>
       </div>
       <div
@@ -33,6 +41,20 @@ function App() {
         <JsonBox />
       </div>
     </div>
+  );
+}
+function renderTooltip(props: any) {
+  return (
+    <Tooltip id="button-tooltip" {...props}>
+      Hi stranger!
+      <br /> Thanks for using jsonmi.
+      <br /> <br />
+      To format your JSON:
+      <br />
+      <b>1.</b> Paste some valid JSON into the box
+      <br />
+      <b>2.</b> $$$
+    </Tooltip>
   );
 }
 
